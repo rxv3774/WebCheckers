1815ae93aab2a6a6acc58550cbff463061c7f235
Ryan Vasquez <rxv3774@rit.edu>
2018-10-12 14:47:46 -0400

    Created PlayerLobby functions to ensure that a valid player may be added to the lobby. Also allows you to retrieve data from the players in the lobby.

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -8,11 +8,53 @@ import java.util.ArrayList;
  * Created by Brett Patterson on 10/11/2018.
  */
 public class PlayerLobby {
+    /**
+     * Array of all the players in the current lobby.
+     */
     private ArrayList<Player> players = new ArrayList<>();
 
+    /**
+     * Attempts to add a player to the array of players. Only works if new username is not already taken.
+     * @param player The new player that is being added.
+     */
     public void addPlayer(Player player) {
-        players.add(player);
+        if (isValidName(player.getUsername())) {
+            players.add(player);
+            System.out.println(player.getUsername() + " has been added"); // Print to website, not console
+
+        }
+        else {
+            System.out.println("Username is already taken");
+        }
     }
 
+    /**
+     * Checks to see if the username name that is being created is already in use by another player.
+     * @param username The username of the player that is trying to be created.
+     * @return Whether or not the user is taken.
+     */
+    public boolean isValidName(String username) {
+        for (Player player : players) {
+            if (player.getUsername().equals(username)) {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    /**
+     * Prints out the list of all the players in the lobby.
+     * @return List of players.
+     */
+    public ArrayList<Player> printPlayers() { //TODO: fix so that it prints to the to the website, not the console.
+        return players;
+    }
+
+    /**
+     * Gets the number of players in the lobby.
+     * @return Size of lobby.
+     */
+    public int getLobbySize() {
+        return players.size();
+    }
 }
--- src/main/java/com/webcheckers/model/Player.java
+++ src/main/java/com/webcheckers/model/Player.java
@@ -2,11 +2,24 @@ package com.webcheckers.model;
 
 public class Player {
 
+    /**
+     * The players username.
+     */
     private String username;
 
+    /**
+     * Player constructor
+     * @param username
+     */
     public Player(String username) {
         this.username = username;
     }
 
-
+    /**
+     * Gets the Player's username.
+     * @return Player username.
+     */
+    public String getUsername() {
+        return this.username;
+    }
 }
