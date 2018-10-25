79f5aba5e2ec3d67bb14b16e14497a2a8a89e93d
NeutronChicken <hclarson@rochester.rr.com>
2018-10-25 16:35:19 -0700

    Shifted responsibility of handle to PostSignInRoute from PlayerLobby and created PlayerLobbyTester

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,17 +1,11 @@
 package com.webcheckers.appl;
 
 import com.webcheckers.model.Player;
-import com.webcheckers.ui.WebServer;
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
 import spark.Session;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import static spark.Spark.halt;
 
 
 /*
@@ -33,46 +27,48 @@ public class PlayerLobby {
 
     /**
      * Attempts to add a player to the array of players. Only works if new username is not already taken.
+     *
      * @param player The new player that is being added.
      */
     public void addPlayer(Player player) {
-        if( isValidName( player.getName() ) ) {
-            players.add(player);
+        players.add(player);
 //            System.out.println( player.getName() + " has been added" ); // Print to website, not console
-        }
+    }
+
+    public void addPlayer(Player player, Session session) {
+        players.add(player);
+        sessionMap.put(player.getName(), session);
     }
 
 
     /**
      * Desc: This method determines if the playerName fallows our naming convention.
+     *
      * @param name the name that needs to be checked
      * @return true if it fallows our rules, false otherwise
      */
-    private boolean isValidName(String name) {
+    public boolean isValidName(String name) {
+        final String allowedChars = "abcdefghijklmnopqrtsuvwyxzABCDEFGHIJKLMNOPQRTSUVWYXZ0123456789 ";
+        boolean allowed = true;
 
-        if( name.equals( "" ) ){
-            return false;
-        }
-
-        String allowed = "abcdefghijklmnopqrtsuvwyxzABCDEFGHIJKLMNOPQRTSUVWYXZ0123456789 ";
-
-        for( int i = 0; i < name.length(); i++){
-            if( !allowed.contains( name.substring(i, i+1) )){
-                return false;
+        if (!name.equals("")) {
+            for (String c : name.split("")) {
+                if (!allowedChars.contains(c)) allowed = false;
             }
-        }
-        return true;
+        } else allowed = false;
+
+        return allowed;
     }
 
     /**
      * Checks to see if the name name that is being created is already in use by another player.
+     *
      * @param name The name of the player that is trying to be created.
      * @return Whether or not the user is taken.
      */
-    public boolean playerNameInUse(String name){
-
-        for( Player player : players) {
-            if( player.getName().equals( name )) {
+    public boolean playerNameInUse(String name) {
+        for (Player player : players) {
+            if (player.getName().equals(name)) {
                 return true;
             }
         }
@@ -82,9 +78,11 @@ public class PlayerLobby {
 
     /**
      * Prints out the list of all the players in the lobby.
+     *
      * @return List of players.
      */
-    public ArrayList<Player> printPlayers() { //TODO: fix so that it prints to the to the website, not the console.
+    //TODO - Fix so that it prints to the to the website, not the console.
+    public ArrayList<Player> printPlayers() {
         return players;
     }
 
@@ -92,8 +90,8 @@ public class PlayerLobby {
     public ArrayList<String> getPlayersNames() {
         ArrayList<String> names = new ArrayList<>();
 
-        for( Player player : players){
-            names.add( player.getName() );
+        for (Player player : players) {
+            names.add(player.getName());
         }
 
         return names;
@@ -102,9 +100,9 @@ public class PlayerLobby {
     /*
      * return player object given name of player
      */
-    public Player getPlayerObject(String name){
-        for (int i=0; i<players.size(); i++){
-            if(players.get(i).getName().equals(name))
+    public Player getPlayerObject(String name) {
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).getName().equals(name))
                 return players.get(i);
         }
         return null;
@@ -113,58 +111,16 @@ public class PlayerLobby {
 
     /**
      * Desc: Gets the number of players in the lobby.
+     *
      * @return Size of lobby.
      */
     public int getLobbySize() {
         return players.size();
     }
 
-    /*
-     *Signin the user if their chosen username is valid. Store the players list and current player
-     * name in the session.
-     *
-     * @param name: The chosen name of the player who wants to sign in
-     * @param request:
-     */
-    public ModelAndView playerSignInProcess(String name, Request request, Response response, Map<String, Object > vm){
-
-        ModelAndView mv;
-
-        if( !isValidName( name ) ){ // invalid name (blank)
-            mv = invalidPlayerName( vm );
-            return mv;
-        }
-
-        if( playerNameInUse( name ) ){
-
-            return playerNameUsedAlready( vm );
-        }
-        if( !playerNameInUse( name ) ){ // No one is using this name.
-
-            Player newPlayer = new Player( name );
-
-            if( !players.contains( newPlayer ) ) {
-                Session httpSession = request.session();
-                httpSession.attribute("playerNames", players);
-                httpSession.attribute("name", name);
-                sessionMap.put( name, httpSession );
-            }
-
-            addPlayer( newPlayer );
-
-            response.redirect( WebServer.HOME_URL );
-            halt();
-            return null;
-
-        }
-
-        System.out.println("reached this.... This is bad");
-        return null;
-    }
-
-
     /**
      * Desc: gets the players session
+     *
      * @param playerName the players name to be used to get the session
      * @return returns the players session
      */
@@ -172,14 +128,14 @@ public class PlayerLobby {
         return sessionMap.get(playerName);
     }
 
-    public String getPlayerNameLst(String name){
+    public String getPlayerNameLst(String name) {
 
 
-        ArrayList<String> playerNameLst = new ArrayList<>( getPlayersNames() );
-        playerNameLst.remove( name );
+        ArrayList<String> playerNameLst = new ArrayList<>(getPlayersNames());
+        playerNameLst.remove(name);
 
         String names = "";
-        if( playerNameLst.size() > 0) {
+        if (playerNameLst.size() > 0) {
 
             names += "Other Players signed in: ";
 
@@ -191,38 +147,9 @@ public class PlayerLobby {
                     names += playerNameLst.get(x);
                 }
             }
-        }
-        else {
+        } else {
             names = "Number of players signed in: " + getLobbySize();
         }
         return names;
     }
-
-
-    /*
-     * Error message for when requested user name contains illegal characters
-     *
-     * @param vm: virtual map for the modelandview
-     * @return ModelAndView: updated modelandview with error message
-     */
-    private ModelAndView invalidPlayerName(Map<String, Object> vm){
-        vm.put( "title", "Sign-In" );
-        vm.put("messageType", "error" );
-        vm.put( "showErrorMessage", "you entered illegal characters in the name. Please enter a different name");
-        return new ModelAndView(vm, VIEW_NAME);
-    }
-
-    /*
-     * Error message for when requested user name is already in use
-     *
-     * @param vm: virtual map for the modelandview
-     * @return ModelAndView: updated modelandview with error message
-     */
-    private ModelAndView playerNameUsedAlready(Map<String, Object> vm){
-        vm.put( "title", "Sign-In" );
-        vm.put("messageType", "error" );
-        vm.put( "showErrorMessage", "you entered an already used name. Please enter a different name");
-        return new ModelAndView(vm, VIEW_NAME);
-    }
-
 }
