5035cd30a0951aa03a6489ee71af4c74242763cd
Samuel Adams <swa1317@rit.edu>
2018-10-25 00:38:51 -0400

    BoardTest complete

--- src/test/java/com/webcheckers/model/BoardTest.java
+++ src/test/java/com/webcheckers/model/BoardTest.java
@@ -1,7 +1,10 @@
 package com.webcheckers.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,12 +13,43 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 @Tag("Model-tier")
 class BoardTest {
+    private Row board[];
 
-    @Test
-    void iterator() {
+    /**
+     * Setup new mock objects for each test.
+     */
+    @BeforeEach
+    public void setup() {
+        board = new Row[8];
     }
 
+    /**
+     * Tests that the constructor functions correctly
+     */
     @Test
-    void getRowsSize() {
+    void test_constructor() {
+        final Board board1 = new Board();
+        assertEquals(board1.getRowsSize(), board.length);
     }
+
+    /*
+     * Tests that the Iterator hasNext functions correctly
+     */
+    @Test
+    void test_hasNext(){
+        final Board board1 = new Board();
+        Iterator itr = board1.iterator();
+        assertTrue(itr.hasNext());
+    }
+
+    /*
+     * Tests that the Iterator next functions correctly
+     */
+    @Test
+    void test_next() {
+        final Board board1 = new Board();
+        Iterator itr = board1.iterator();
+        assertEquals(board, itr.next());
+    }
+
 }
